7060efffaac6f12e9e64c704f5b2789a7e582d60
Snehal Navale <navalesnehal21@gmail.com>
2023-07-04 16:18:12 +0530

    admin panel data

--- server.js
+++ server.js
@@ -1,82 +1,112 @@
-function register(event){
+function register(event) {
     event.preventDefault();
     // alert("working")
 
-    var name=document.getElementById("tname").value;
-    var email=document.getElementById("temail").value;
-    var password=document.getElementById("tpassword").value;
-    var confirmpassword=document.getElementById("tconfirmpassword").value;
+    var name = document.getElementById("tname").value;
+    var email = document.getElementById("temail").value;
+    var password = document.getElementById("tpassword").value;
+    var confirmpassword = document.getElementById("tconfirmpassword").value;
     // var cartproducts=[];
-    if(name && email && password && confirmpassword){
-        if(password.length>=8 && confirmpassword.length>=8){
-            if(password==confirmpassword){
-                var tarray=JSON.parse(localStorage.getItem("tanishqusers")) || []
-                var flag=false;
-                for(var i=0;i<tarray.length;i++){
-                    if(tarray[i].uemail==email){
-                        flag=true;
+    if (name && email && password && confirmpassword) {
+        if (password.length >= 8 && confirmpassword.length >= 8) {
+            if (password == confirmpassword) {
+                var tarray = JSON.parse(localStorage.getItem("tanishqusers")) || []
+                var flag = false;
+                for (var i = 0; i < tarray.length; i++) {
+                    if (tarray[i].uemail == email) {
+                        flag = true;
                     }
                 }
-                if(flag==true){
+                if (flag == true) {
                     alert("email alredy exist.")
-                   
+
                 }
-                else{
-                    var tanishq={uname:name,uemail:email,upassword:password,uconfirmpassword:confirmpassword}
+                else {
+                    var tanishq = { uname: name, uemail: email, upassword: password, uconfirmpassword: confirmpassword }
                     tarray.push(tanishq);
-                    localStorage.setItem("tanishqusers",JSON.stringify(tarray));
+                    localStorage.setItem("tanishqusers", JSON.stringify(tarray));
                     alert("Signed up successfully");
-                    document.getElementById("tname").value=''
-                    document.getElementById("temail").value=''
-                    document.getElementById("tpassword").value=''
-                    document.getElementById("tconfirmpassword").value=''
-                    window.location.href="./login.html"
+                    document.getElementById("tname").value = ''
+                    document.getElementById("temail").value = ''
+                    document.getElementById("tpassword").value = ''
+                    document.getElementById("tconfirmpassword").value = ''
+                    window.location.href = "./login.html"
                 }
             }
-            else{
+            else {
                 console.log("password not matched.")
             }
         }
-        else{
+        else {
             console.log("Password should be minimum 8 numbers.")
         }
 
     }
-    else{
+    else {
         console.log("all fields are required.")
     }
 }
 
 
-function login(event){
+function login(event) {
     event.preventDefault();
     // alert("working")
-    var lemail=document.getElementById("lemail").value;
-    var lpassword=document.getElementById("lpassword").value;
-    var loginuser={};
-    if(lemail && lpassword){
-        var tanishq=JSON.parse(localStorage.getItem("tanishqusers"));
-       var flag=false;
-        for(var i=0;i<tanishq.length;i++){
-            if(tanishq[i].uemail==lemail && tanishq[i].upassword==lpassword){
-                flag=true;
-                loginuser=tanishq[i];
+    var lemail = document.getElementById("lemail").value;
+    var lpassword = document.getElementById("lpassword").value;
+    var loginuser = {};
+    if (lemail && lpassword) {
+        var tanishq = JSON.parse(localStorage.getItem("tanishqusers"));
+        var flag = false;
+        for (var i = 0; i < tanishq.length; i++) {
+            if (tanishq[i].uemail == lemail && tanishq[i].upassword == lpassword) {
+                flag = true;
+                loginuser = tanishq[i];
             }
         }
-        if(flag==true){
+        if (flag == true) {
             // tanishq[i]=loginuser;
             // tanishq.push(loginuser);
-            localStorage.setItem("tanishqlogin",JSON.stringify(loginuser));
+            localStorage.setItem("tanishqlogin", JSON.stringify(loginuser));
             alert("logged in successfully");
-            document.getElementById("lemail").value=''
-            document.getElementById("lpassword").value=''
-            window.location.href="./home.html"
+            document.getElementById("lemail").value = ''
+            document.getElementById("lpassword").value = ''
+            window.location.href = "./home.html"
         }
-        else{
+        else {
             alert("credential not matched")
         }
     }
-    else{
+    else {
         console.log("Both Fields are required.")
+    }
+}
+
+
+function adminLogin(event) {
+    event.preventDefault();
+
+    let email = document.getElementById("adminEmail").value;
+    let password = document.getElementById("adminPassword").value;
+    console.log(email,password);
+    let adminLogin = {};
+    if (email && password) {
+        let flag = false;
+        let adminStorage = JSON.parse(localStorage.getItem("adminUsers"));
+        for (let i = 0; i < adminStorage.length; i++) {
+            if (email == adminStorage[i].email && password == adminStorage[i].password) {
+                flag = true;
+                adminLogin = adminStorage[i];
+            }
+        }
+        if (flag==true) {
+            localStorage.setItem("adminLogin", JSON.stringify(adminLogin));
+            document.getElementById("adminEmail").value='';
+            document.getElementById("adminPassword").value='';
+            window.location.href='./adminPanel.html';
+        }else{
+            alert("Credentials not matched.");
+        }
+    } else {
+        alert("All fields are mandatory.")
     }
 }
